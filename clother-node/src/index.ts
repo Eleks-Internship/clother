3a2b6d4e95e95f91d417ca15c16120c9b30a9be5
sergiy-ta <serhiy819@gmail.com>
2021-01-27 11:23:48 +0200

    added been node

---
+++ clother-node/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+const app: express.Application = express();
+const port: string | number = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../../clother-angular/dist/clother-angular')));
+
+app.get('**', (req: express.Request, res: express.Response) => {
+    res.status(200).sendFile(path.join(__dirname, '../../clother-angular/dist/clother-angular/index.html'));
+});
+
+const server: http.Server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is starting: ${port}`);
+});
