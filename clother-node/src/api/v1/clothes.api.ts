e5e7343f2a8bda390c80fc3c7dc5ae436237b6f2
sergiy-ta <serhiy819@gmail.com>
2021-02-01 18:35:33 +0200

    added been api for get clothes

--- clother-node/src/api/v1/clothes.api.ts
+++ clother-node/src/api/v1/clothes.api.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import APIService from '../../services/api.service';
 import ClothesService from '../../services/clothes/clothes.service';
+import { ObjectID } from 'mongodb';
 
 const router: express.Router = express.Router();
 
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
+
+router.get('/clothes/:id', async (req: express.Request, res: express.Response) => {
+    const clothesService: ClothesService = new ClothesService();
+    APIService.processingOnAPIOfDataModels({ req, res, method: clothesService.get(new ObjectID(req.params.id)), dataError: null});
+});
 
 router.post('/clothes', async (req: express.Request, res: express.Response) => {
     if (!req.body) return res.sendStatus(400);
