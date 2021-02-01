a22c5b79fbbc5ebcca1d0850c3595746e47bc2f8
sergiy-ta <serhiy819@gmail.com>
2021-02-01 18:37:03 +0200

    added been test for method get in class clothes service

--- clother-node/test/services/clothes/clothes.service.test.ts
+++ clother-node/test/services/clothes/clothes.service.test.ts
@@ -2,15 +2,30 @@ import { expect } from 'chai';
 
 import ClothesService from '../../../src/services/clothes/clothes.service';
 import Clothes from '../../../src/interface/object/clothes';
+import { ObjectID } from 'mongodb';
 
 describe('Test service clothes', () => {
     const name: string = "test"
     const photoName: string = "test";
     const urlForBuy: string = "test";
+    let _id: ObjectID;
 
     it('create', async () => {
         const clothesService: ClothesService = new ClothesService("clothes-test");
         const clothes: Clothes | null = await clothesService.create({ name, photoName, urlForBuy });
+        if (clothes) {
+            expect(clothes.name).to.eql(name);
+            expect(clothes.photoName).to.eql(photoName);
+            expect(clothes.urlForBuy).to.eql(urlForBuy);
+            _id = clothes._id;
+        } else {
+            expect(clothes).to.not.eql(null);
+        }
+    });
+
+    it('get', async () => {
+        const clothesService: ClothesService = new ClothesService("clothes-test");
+        const clothes: Clothes | null = await clothesService.get(_id);
         if (clothes) {
             expect(clothes.name).to.eql(name);
             expect(clothes.photoName).to.eql(photoName);
