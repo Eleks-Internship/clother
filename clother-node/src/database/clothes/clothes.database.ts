6849c68124639c260a4c63b795a421fdb13958e0
sergiy-ta <serhiy819@gmail.com>
2021-02-01 18:34:53 +0200

    added been get method to class for work with database

--- clother-node/src/database/clothes/clothes.database.ts
+++ clother-node/src/database/clothes/clothes.database.ts
@@ -1,9 +1,11 @@
+import { ObjectID } from "mongodb";
 import Create from "../../interface/class/create";
+import Get from "../../interface/class/get";
 import Clothes from "../../interface/object/clothes";
 import Database from "../database";
 import { dbClothes } from "../database_info";
 
-export default class ClothesDatabase extends Database<Clothes> implements Create<Clothes> {
+export default class ClothesDatabase extends Database<Clothes> implements Create<Clothes>, Get<Clothes> {
     private static collection: string;
 
     constructor(collection: string = "clothes") {
@@ -14,5 +16,9 @@ export default class ClothesDatabase extends Database<Clothes> implements Create
 
     public create(info: { name: string, photoName: string, urlForBuy: string }): Promise<Clothes | null> {
         return super.create(info);
+    }
+
+    public get(_id: ObjectID): Promise<Clothes | null> {
+        return super.get(_id);
     }
 }
