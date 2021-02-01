9f6860b9480ac7483bb26108f4688758edc4a32b
sergiy-ta <serhiy819@gmail.com>
2021-02-01 18:35:16 +0200

    added been get method to service class for clothes

--- clother-node/src/services/clothes/clothes.service.ts
+++ clother-node/src/services/clothes/clothes.service.ts
@@ -1,8 +1,10 @@
+import { ObjectID } from "mongodb";
 import ClothesDatabase from "../../database/clothes/clothes.database";
 import Create from "../../interface/class/create";
+import Get from "../../interface/class/get";
 import Clothes from "../../interface/object/clothes";
 
-export default class ClothesService implements Create<Clothes> {
+export default class ClothesService implements Create<Clothes>, Get<Clothes> {
     private readonly clothesDatabase: ClothesDatabase;
 
     constructor(collection: string = "clothes") {
@@ -11,5 +13,9 @@ export default class ClothesService implements Create<Clothes> {
 
     public async create(info: { name: string, photoName: string, urlForBuy: string }): Promise<Clothes | null> {
         return this.clothesDatabase.create(info);
+    }
+
+    public async get(_id: ObjectID): Promise<Clothes | null> {
+        return this.clothesDatabase.get(_id);
     }
 }
