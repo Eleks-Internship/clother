2115730c8233a34c939dffd0240e85a547f486fa
sergiy-ta <serhiy819@gmail.com>
2021-02-04 18:29:07 +0200

    added get method to class user database

--- clother-node/src/database/user/user.database.ts
+++ clother-node/src/database/user/user.database.ts
@@ -1,14 +1,20 @@
+import { ObjectID } from "mongodb";
 import Create from "../../interface/class/create";
+import Get from "../../interface/class/get";
 import User from "../../interface/object/user";
 import Database from "../database";
 import { dbUser } from "../database_info";
 
-export default class UserDatabase extends Database<User> implements Create<User> {
+export default class UserDatabase extends Database<User> implements Create<User>, Get<User> {
     constructor(collection: string = "user") {
         super({ database: dbUser, collection });
     }
 
     public create(info: { firstName: string, lastName: string, email: string, password: string }): Promise<User | null> {
         return super.create(info);
+    }
+
+    public get(_id: ObjectID): Promise<User | null> {
+        return super.get(_id);
     }
 }
