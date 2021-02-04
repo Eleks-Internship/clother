5ffaaf155cfbe7f58696e9eb896e3b48e2860ef9
sergiy-ta <serhiy819@gmail.com>
2021-02-04 18:30:30 +0200

    added test for get method in class user service

--- clother-node/test/services/user/user.service.test.ts
+++ clother-node/test/services/user/user.service.test.ts
@@ -2,12 +2,14 @@ import { expect } from 'chai';
 
 import UserService from '../../../src/services/user/user.service';
 import User from '../../../src/interface/object/user';
+import { ObjectID } from 'mongodb';
 
 describe('Test service user', () => {
     const firstName: string = "firstName";
     const lastName: string = "lastName";
     const email: string = "email@email.com";
     const password: string = "password";
+    let _id: ObjectID;
 
     it('create', async () => {
         const userService: UserService = new UserService("user-test");
@@ -16,8 +18,21 @@ describe('Test service user', () => {
             expect(user.firstName).to.eql(firstName);
             expect(user.lastName).to.eql(lastName);
             expect(user.email).to.eql(email);
+            _id = user._id;
         } else {
             expect(user).to.not.eql(null);
         }
-    })
+    });
+
+    it('get', async () => {
+        const userDatabase: UserService = new UserService("user-test");
+        const user: User | null = await userDatabase.get(_id);
+        if (user) {
+            expect(user.firstName).to.eql(firstName);
+            expect(user.lastName).to.eql(lastName);
+            expect(user.email).to.eql(email);
+        } else {
+            expect(user).to.not.eql(null);
+        }
+    });
 });
