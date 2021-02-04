3811910dc04dd257c4812c4a8e2f2abe5c31609d
sergiy-ta <serhiy819@gmail.com>
2021-02-04 18:30:11 +0200

    added test for get method in class user database

--- clother-node/test/database/user/user.database.test.ts
+++ clother-node/test/database/user/user.database.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { ObjectID } from 'mongodb';
 
 import UserDatabase from '../../../src/database/user/user.database';
 import User from '../../../src/interface/object/user';
@@ -8,6 +9,7 @@ describe('Test database user', () => {
     const lastName: string = "lastName";
     const email: string = "email@email.com";
     const password: string = "password";
+    let _id: ObjectID;
 
     it('create', async () => {
         const userDatabase: UserDatabase = new UserDatabase("user-test");
@@ -17,8 +19,22 @@ describe('Test database user', () => {
             expect(user.lastName).to.eql(lastName);
             expect(user.email).to.eql(email);
             expect(user.password).to.eql(password);
+            _id = user._id;
         } else {
             expect(user).to.not.eql(null);
         }
-    })
+    });
+
+    it('get', async () => {
+        const userDatabase: UserDatabase = new UserDatabase("user-test");
+        const user: User | null = await userDatabase.get(_id);
+        if (user) {
+            expect(user.firstName).to.eql(firstName);
+            expect(user.lastName).to.eql(lastName);
+            expect(user.email).to.eql(email);
+            expect(user.password).to.eql(password);
+        } else {
+            expect(user).to.not.eql(null);
+        }
+    });
 });
