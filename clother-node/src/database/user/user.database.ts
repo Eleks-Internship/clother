3a7a286fab7c3c8fecf2ad152bb312320ac2ecaf
sergiy-ta <serhiy819@gmail.com>
2021-02-04 20:49:55 +0200

    added update method to class user database

--- clother-node/src/database/user/user.database.ts
+++ clother-node/src/database/user/user.database.ts
@@ -1,11 +1,12 @@
 import { ObjectID } from "mongodb";
 import Create from "../../interface/class/create";
 import Get from "../../interface/class/get";
+import Update from "../../interface/class/update";
 import User from "../../interface/object/user";
 import Database from "../database";
 import { dbUser } from "../database_info";
 
-export default class UserDatabase extends Database<User> implements Create<User>, Get<User> {
+export default class UserDatabase extends Database<User> implements Create<User>, Get<User>, Update {
     constructor(collection: string = "user") {
         super({ database: dbUser, collection });
     }
@@ -16,5 +17,9 @@ export default class UserDatabase extends Database<User> implements Create<User>
 
     public get(_id: ObjectID): Promise<User | null> {
         return super.get(_id);
+    }
+
+    public update(info: { _id: ObjectID, data: { firstName: string, lastName: string, email: string, password: string } }): Promise<boolean> {
+        return super.update(info);
     }
 }
