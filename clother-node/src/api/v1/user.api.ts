1c7b76f812f8e7432e56cacec02379cf71f4ffbe
sergiy-ta <serhiy819@gmail.com>
2021-02-04 18:29:42 +0200

    added get method to api user

--- clother-node/src/api/v1/user.api.ts
+++ clother-node/src/api/v1/user.api.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import APIService from '../../services/api.service';
 import LoginService from '../../services/auth/login.service';
+import UserService from '../../services/user/user.service';
+import { ObjectID } from 'mongodb';
 
 const router: express.Router = express.Router();
 
@@ -15,6 +17,16 @@ router.post('/users', (req: express.Request, res: express.Response) => {
 
     try {
         APIService.processingOnAPIOfDataModels({ req, res, method: loginService.getTokenAfterCreateUser(req.body), dataError: null });
+    } catch (error) {
+        APIService.catchError({ res, req, error, dataError: null });
+    }
+});
+
+router.get('/users/:id', (req: express.Request, res: express.Response) => {
+    const userService: UserService = new UserService();
+
+    try {
+        APIService.processingOnAPIOfDataModels({ req, res, method: userService.get(new ObjectID(req.params.id)), dataError: null });
     } catch (error) {
         APIService.catchError({ res, req, error, dataError: null });
     }
