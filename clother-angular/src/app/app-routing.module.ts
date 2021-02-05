113f400eda9a19f4871bfff351d535970adadcbe
sergiy-ta <serhiy819@gmail.com>
2021-02-05 18:58:27 +0200

    added sign up to routing

--- clother-angular/src/app/app-routing.module.ts
+++ clother-angular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-up', component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
