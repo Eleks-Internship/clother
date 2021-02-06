2c803406c66eeff024a3beae25f4b105f6e0e53a
sergiy-ta <serhiy819@gmail.com>
2021-02-06 18:15:16 +0200

    added user to interface of clothes

--- clother-node/src/interface/object/clothes.ts
+++ clother-node/src/interface/object/clothes.ts
@@ -1,9 +1,11 @@
 import { ObjectID } from "mongodb";
+import User from "./user";
 
 export default interface Clothes {
     readonly _id: ObjectID;
     name: string;
     photoName: string;
     urlForBuy?: string;
+    readonly user: User | { _id: ObjectID; };
     readonly dateOfCreation: Date;
 }
