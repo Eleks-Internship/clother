f139e5e323dcd530c29dec0c254ac160e185580f
sergiy-ta <serhiy819@gmail.com>
2021-02-06 21:51:52 +0200

    added test for method post for api login

---
+++ clother-node/test/api/login/login.test.ts
@@ -0,0 +1,29 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import 'mocha';
+
+chai.use(chaiHttp);
+
+const server: string = process.env.SERVER || 'localhost';
+const port: string | number = process.env.PORT || 3000;
+
+describe('Test api login', () => {
+    const email: string = "test@test.com";
+    const password: string = "test";
+
+    it('post', async () => {
+        return chai.request('http://' + server +':' + port + '/api').post('/v1/login').send({
+            email,
+            password
+        }).then(res => {
+            chai.expect(res.status).to.eql(200);
+            if (res.body) {
+                chai.expect(res.body.data).to.not.eql(null);
+                chai.expect(res.body.data).to.not.eql('');
+                chai.expect(res.body.data).to.be.a('string');
+            } else {
+                chai.expect(res.body).to.not.eql(null);
+            }
+        });
+    });
+});
