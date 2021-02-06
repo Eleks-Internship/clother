c2b18cd66bf1a275c10e7536afad3630b72e49a0
sergiy-ta <serhiy819@gmail.com>
2021-02-06 21:48:51 +0200

    added method post to api login

---
+++ clother-node/src/api/v1/login.api.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import LoginService from '../../services/auth/login.service';
+import APIService from '../../services/api.service';
+
+const router: express.Router = express.Router();
+
+router.post('/login', (req: express.Request, res: express.Response) => {
+    const loginService: LoginService = new LoginService();
+
+    try {
+        APIService.processingOnAPIOfDataModels({req, res, method: loginService.getToken(req.body), dataError: ''});
+    } catch (error) {
+        APIService.catchError({res, req, error, dataError: ''});
+    }
+});
+
+export default router;
