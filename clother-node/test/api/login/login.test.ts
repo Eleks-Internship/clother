9293c2fc6998b5df7347b98f1fb1c0f1a6657a57
sergiy-ta <serhiy819@gmail.com>
2021-02-09 19:13:24 +0200

    added test for method post for login thanks by facebook in api

--- clother-node/test/api/login/login.test.ts
+++ clother-node/test/api/login/login.test.ts
@@ -8,6 +8,8 @@ const server: string = process.env.SERVER || 'localhost';
 const port: string | number = process.env.PORT || 3000;
 
 describe('Test api login', () => {
+    const firstName: string = 'firstName';
+    const lastName: string = 'lastName';
     const email: string = "test@test.com";
     const password: string = "test";
 
@@ -15,6 +17,23 @@ describe('Test api login', () => {
         return chai.request('http://' + server +':' + port + '/api').post('/v1/login').send({
             email,
             password
+        }).then(res => {
+            chai.expect(res.status).to.eql(200);
+            if (res.body) {
+                chai.expect(res.body.data).to.not.eql(null);
+                chai.expect(res.body.data).to.not.eql('');
+                chai.expect(res.body.data).to.be.a('string');
+            } else {
+                chai.expect(res.body).to.not.eql(null);
+            }
+        });
+    });
+
+    it('post facebook', async () => {
+        return chai.request('http://' + server +':' + port + '/api').post('/v1/login/facebook').send({
+            firstName,
+            lastName,
+            email
         }).then(res => {
             chai.expect(res.status).to.eql(200);
             if (res.body) {
