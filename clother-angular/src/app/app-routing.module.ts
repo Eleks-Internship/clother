e25d0b62bc0169aa5aaa1eb8aad69178402897ee
sergiy-ta <serhiy819@gmail.com>
2021-02-11 18:03:09 +0200

    added login guard

--- clother-angular/src/app/app-routing.module.ts
+++ clother-angular/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from './guards/login.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
