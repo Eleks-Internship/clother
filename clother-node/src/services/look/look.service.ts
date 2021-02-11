6324cb99b61214f8f61ae09ec4f166489adc1905
sergiy-ta <serhiy819@gmail.com>
2021-02-12 00:14:19 +0200

    added method get list for class service look

--- clother-node/src/services/look/look.service.ts
+++ clother-node/src/services/look/look.service.ts
@@ -1,9 +1,10 @@
 import { ObjectID } from "mongodb";
 import LookDatabase from "../../database/look/look.database";
 import Create from "../../interface/class/create";
+import GetList from "../../interface/class/get_list";
 import Look from "../../interface/object/look";
 
-export default class LookService implements Create<Look> {
+export default class LookService implements Create<Look>, GetList<Look> {
     private readonly lookDatabase: LookDatabase;
     constructor(collection: string = "look") {
         this.lookDatabase = new LookDatabase(collection);
@@ -11,5 +12,9 @@ export default class LookService implements Create<Look> {
 
     public create(info: { name: string, clothes: { _id: ObjectID }[], user: { _id: ObjectID }}): Promise<Look | null> {
         return this.lookDatabase.create(info);
+    }
+
+    public getList(): Promise<Look[]> {
+        return this.lookDatabase.getList();
     }
 }
