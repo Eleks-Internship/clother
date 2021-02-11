743cb659a56b771cdc2cbce1c783eaef0b6a7171
sergiy-ta <serhiy819@gmail.com>
2021-02-12 00:15:51 +0200

    added test for method get list in class service look

--- clother-node/test/services/look/look.service.test.ts
+++ clother-node/test/services/look/look.service.test.ts
@@ -24,4 +24,17 @@ describe('Test service look', () => {
             expect(clothes).to.not.eql(null);
         }
     });
+
+    it('get list', async () => {
+        const lookService: LookService = new LookService("look-test");
+        const lookList: Look[] = await lookService.getList();
+
+        CollectionDatabase.deleteUser();
+
+        expect(lookList).to.not.eql(null);
+        expect(lookList.length).to.not.eql(0);
+        expect(lookList[0].name).to.eql(name);
+        expect(lookList[0].clothes).to.eql(clothes);
+        expect(lookList[0].user).to.eql(user);
+    });
 });
