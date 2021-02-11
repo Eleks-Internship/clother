0dc1063d71f1904e01ffe0429907bcac7c7d74fb
sergiy-ta <serhiy819@gmail.com>
2021-02-12 00:14:46 +0200

    added method get list to api look

--- clother-node/src/api/v1/look.api.ts
+++ clother-node/src/api/v1/look.api.ts
@@ -9,6 +9,16 @@ const router: express.Router = express.Router();
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
+router.get('/looks', (req: express.Request, res: express.Response) => {
+    const lookService: LookService = new LookService();
+
+    try {
+        APIService.processingOnAPIOfDataModels({ req, res, method: lookService.getList(), dataError: [] });
+    } catch(error) {
+        APIService.catchError({ req, res, error, dataError: [] });
+    }
+});
+
 router.post('/looks', async (req: express.Request, res: express.Response) => {
     const lookService: LookService = new LookService();
 
@@ -18,7 +28,7 @@ router.post('/looks', async (req: express.Request, res: express.Response) => {
     }));
 
     try {
-        APIService.processingOnAPIOfDataModels({ req, res, method: lookService.create({ name: req.body.name, clothes, user: { _id: new ObjectID(req.body.user._id) } }), dataError: null });
+        APIService.processingOnAPIOfDataModels({ req, res, method: lookService.create({ name: req.body.name, clothes: clothesList, user: { _id: new ObjectID(req.body.user._id) } }), dataError: null });
     } catch(error) {
         APIService.catchError({ req, res, error, dataError: null });
     }
