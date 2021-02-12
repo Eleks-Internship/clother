8dea60584e77e8e84a33d170da1ebc547d608fba
sergiy-ta <serhiy819@gmail.com>
2021-02-12 11:06:00 +0200

    added test for method update in class look database

--- clother-node/test/database/look/look.database.test.ts
+++ clother-node/test/database/look/look.database.test.ts
@@ -7,14 +7,16 @@ import CollectionDatabase from '../../../src/database/collection/collection.data
 
 describe('Test database look', () => {
     const user: { _id: ObjectID } = { _id: new ObjectID('6015a18c923e0e3b6442751c') };
-    const name: string = "test"
-    const clothes: { _id: ObjectID }[] = [{ _id: new ObjectID('6015a18c923e0e3b6442751c') }];
+    let name: string = "test"
+    let clothes: { _id: ObjectID }[] = [{ _id: new ObjectID('6015a18c923e0e3b6442751c') }];
+    let _id: ObjectID;
 
     it('create', async () => {
         const lookDatabase: LookDatabase = new LookDatabase("look-test");
         const look: Look | null = await lookDatabase.create({ name, clothes, user });
 
         if (look) {
+            _id = look._id;
             expect(look.name).to.eql(name);
             expect(look.clothes).to.eql(clothes);
             expect(look.user).to.eql(user);
@@ -27,12 +29,20 @@ describe('Test database look', () => {
         const lookDatabase: LookDatabase = new LookDatabase("look-test");
         const lookList: Look[] = await lookDatabase.getList();
 
-        CollectionDatabase.deleteLook();
-
         expect(lookList).to.not.eql(null);
         expect(lookList.length).to.not.eql(0);
         expect(lookList[0].name).to.eql(name);
         expect(lookList[0].clothes).to.eql(clothes);
         expect(lookList[0].user).to.eql(user);
+    });
+
+    it('update', async () => {
+        name = "testUpdate"
+        clothes = [{ _id: new ObjectID('6015a18c923e0e3b6442751c') }, { _id: new ObjectID('6015a18c923e0e3b6442751c') }];
+
+        const lookDatabase: LookDatabase = new LookDatabase("look-test");
+        expect(await lookDatabase.update({ _id, data: { name, clothes } })).eql(true);
+
+        CollectionDatabase.deleteLook();
     });
 });
