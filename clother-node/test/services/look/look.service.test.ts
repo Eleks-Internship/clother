375f7f00a78275bb05c48c707ef2c36809075874
sergiy-ta <serhiy819@gmail.com>
2021-02-12 11:06:33 +0200

    added test for method update in class look service

--- clother-node/test/services/look/look.service.test.ts
+++ clother-node/test/services/look/look.service.test.ts
@@ -7,16 +7,16 @@ import CollectionDatabase from '../../../src/database/collection/collection.data
 
 describe('Test service look', () => {
     const user: { _id: ObjectID } = { _id: new ObjectID('6015a18c923e0e3b6442751c') };
-    const name: string = "test"
-    const clothes: { _id: ObjectID }[] = [{ _id: new ObjectID('6015a18c923e0e3b6442751c') }];
+    let name: string = "test"
+    let clothes: { _id: ObjectID }[] = [{ _id: new ObjectID('6015a18c923e0e3b6442751c') }];
+    let _id: ObjectID;
 
     it('create', async () => {
         const lookService: LookService = new LookService("look-test");
-        const look: Look | null = await lookService.create({ name, clothes, user });
-
-        CollectionDatabase.deleteUser();
+        const look: Look | null = await lookService.create({ name, clothes, user })
         
         if (look) {
+            _id = look._id;
             expect(look.name).to.eql(name);
             expect(look.clothes).to.eql(clothes);
             expect(look.user).to.eql(user);
@@ -29,12 +29,22 @@ describe('Test service look', () => {
         const lookService: LookService = new LookService("look-test");
         const lookList: Look[] = await lookService.getList();
 
-        CollectionDatabase.deleteUser();
-
         expect(lookList).to.not.eql(null);
         expect(lookList.length).to.not.eql(0);
         expect(lookList[0].name).to.eql(name);
         expect(lookList[0].clothes).to.eql(clothes);
         expect(lookList[0].user).to.eql(user);
+    });
+
+
+
+    it('update', async () => {
+        name = "testUpdate"
+        clothes = [{ _id: new ObjectID('6015a18c923e0e3b6442751c') }, { _id: new ObjectID('6015a18c923e0e3b6442751c') }];
+
+        const lookService: LookService = new LookService("look-test");
+        expect(await lookService.update({ _id, data: { name, clothes } })).eql(true);
+
+        CollectionDatabase.deleteUser();
     });
 });
