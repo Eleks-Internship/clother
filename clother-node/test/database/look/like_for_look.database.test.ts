06e6d2c72f85f510d6833ae3a6d96cdb705c7477
sergiy-ta <serhiy819@gmail.com>
2021-02-16 00:04:35 +0200

    added test for method delete in class like for look in database

--- clother-node/test/database/look/like_for_look.database.test.ts
+++ clother-node/test/database/look/like_for_look.database.test.ts
@@ -7,12 +7,14 @@ import LikeForLook from "../../../src/interface/object/like_for_look";
 describe('Test database like for look', () => {
     const user: { _id: ObjectID } = { _id: new ObjectID('6015a18c923e0e3b6442751c') };
     const look: { _id: ObjectID } = { _id: new ObjectID('6015a18c923e0e3b6442751c') };
+    let _id: ObjectID;
 
     it('create', async () => {
         const likeForLookDatabase: LikeForLookDatabase = new LikeForLookDatabase("like-for-look-test");
         const likeForLook: LikeForLook | null = await likeForLookDatabase.create({ user, look });
 
         if (likeForLook) {
+            _id = likeForLook._id;
             expect(likeForLook.user).to.eql(user);
             expect(likeForLook.look).to.eql(look);
         } else {
@@ -24,13 +26,18 @@ describe('Test database like for look', () => {
         const likeForLookDatabase: LikeForLookDatabase = new LikeForLookDatabase("like-for-look-test");
         const likeForLookList: LikeForLook[] = await likeForLookDatabase.getListByKeys({ user, look });
 
-        CollectionDatabase.deleteLook();
-
         if (likeForLookList) {
             expect(likeForLookList[0].user).to.eql(user);
             expect(likeForLookList[0].look).to.eql(look);
         } else {
             expect(likeForLookList).to.not.eql(null);
         }
+    });
+
+    it('delete', async () => {
+        const likeForLookDatabase: LikeForLookDatabase = new LikeForLookDatabase("like-for-look-test");
+        expect(await likeForLookDatabase.delete(_id)).eql(true);
+
+        CollectionDatabase.deleteLook();
     });
 });
