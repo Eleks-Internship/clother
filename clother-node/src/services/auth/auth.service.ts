56424740dc9fb4b586db5318d408e723c3069740
sergiy-ta <serhiy819@gmail.com>
2021-02-15 19:25:15 +0200

    added method verificate token and get user id from token

--- clother-node/src/services/auth/auth.service.ts
+++ clother-node/src/services/auth/auth.service.ts
@@ -1,13 +1,37 @@
 import jsonwebtoken from 'jsonwebtoken';
 
-export default class AuthService {
+interface VerifyToken {
+    access: boolean;
+    authData: { id: string } | undefined;
+}
+
+export default abstract class AuthService {
     private readonly secretKey: string = "elbjlsdhbgelyrbgdhgbw;rigbsd/fnweoigbw';egbdslhjgbslyguwvbgulwebfiuwebf;sd.fbn;eirgbuerg;bsdgbu;iebguer;gubrg";
 
-    public createToken(id: string): Promise<string> {
+    protected createToken(id: string): Promise<string> {
         return new Promise<string>((resolve, reject) => {
             jsonwebtoken.sign({ id }, this.secretKey, (error: any, token: any) => {
                 resolve(token);
                 if (error) reject(error);
+            });
+        });
+    }
+
+    protected verificateToken(token: string): Promise<VerifyToken> {
+        token = token.replace('Bearer ', '');
+
+        return new Promise<VerifyToken>((resolve, reject) => {
+            jsonwebtoken.verify(token, this.secretKey, (error: any, authData: any) => {
+                if (error) {
+                    reject(error);
+                    resolve({ access: false, authData: undefined });
+                } else {
+                    if (authData && authData.hasOwnProperty('id') ) {
+                        resolve({ access: true, authData: { id: authData.id } });
+                    } else {
+                        resolve({ access: false, authData: undefined });
+                    }
+                }
             });
         });
     }
