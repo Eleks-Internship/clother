4eb9160cd43ef11036abb6742767aa2446a3cddc
sergiy-ta <serhiy819@gmail.com>
2021-02-15 19:24:37 +0200

    created service class for like for look and added method create

---
+++ clother-node/src/services/look/like_for_look.service.ts
@@ -0,0 +1,15 @@
+import { ObjectID } from "mongodb";
+import LikeForLookDatabase from "../../database/look/like_for_look.database";
+import LikeForLook from "../../interface/object/like_for_look";
+
+export default class LikeForLookService {
+    private readonly likeForLookDatabase: LikeForLookDatabase;
+
+    constructor(collection: string = 'like-for-look') {
+        this.likeForLookDatabase  = new LikeForLookDatabase(collection);
+    }
+
+    public create(info: { look: { _id: ObjectID; }, user: { _id: ObjectID; }}): Promise<LikeForLook | null> {
+        return this.likeForLookDatabase.create(info);
+    }
+}
