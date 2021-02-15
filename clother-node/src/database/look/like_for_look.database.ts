f37d1e989213817bdcd372ceab9f0f763dbfe2fa
sergiy-ta <serhiy819@gmail.com>
2021-02-16 00:02:59 +0200

    added method delete for class like for look database

--- clother-node/src/database/look/like_for_look.database.ts
+++ clother-node/src/database/look/like_for_look.database.ts
@@ -1,11 +1,12 @@
 import { ObjectID } from "mongodb";
 import Create from "../../interface/class/create";
+import Delete from "../../interface/class/delete";
 import GetListByKeys from "../../interface/class/get_list_by_key";
 import LikeForLook from "../../interface/object/like_for_look";
 import { dbLook } from '../database_info';
 import DatabaseObject from "../database_object";
 
-export default class LikeForLookDatabase extends DatabaseObject<LikeForLook> implements Create<LikeForLook>, GetListByKeys<LikeForLook> {
+export default class LikeForLookDatabase extends DatabaseObject<LikeForLook> implements Create<LikeForLook>, GetListByKeys<LikeForLook>, Delete {
 
     constructor(collection: string = 'like-for-look') {
         super({ database: dbLook, collection });
@@ -17,5 +18,9 @@ export default class LikeForLookDatabase extends DatabaseObject<LikeForLook> imp
 
     public getListByKeys(info: object): Promise<LikeForLook[]> {
         return super.getListByKeys(info);
+    }
+
+    public delete(_id: ObjectID): Promise<boolean> {
+        return super.delete(_id);
     }
 }
