9a6e48382b1cea3f35a76ff692350f7ea9c710a5
bohdan bihun <enigmaautoeinal@gmail.com>
2021-02-16 11:29:24 +0200

    #36 Create main page

---
+++ clother-angular/src/app/components/components/pagemain/pagemain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagemain',
+  templateUrl: './pagemain.component.html',
+  styleUrls: ['./pagemain.component.scss']
+})
+export class PagemainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
