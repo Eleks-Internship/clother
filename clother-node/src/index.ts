3e7448d3cda5b7d133ac5dc25288f64f66b93938
sergiy-ta <serhiy819@gmail.com>
2021-02-18 16:56:09 +0200

    added cors to index file for back end

--- clother-node/src/index.ts
+++ clother-node/src/index.ts
@@ -1,31 +1,16 @@
 import express from 'express';
 import http from 'http';
-import path from 'path';
+import cors from 'cors';
 
 import api from './api/api';
 import LogWithoutDatabaseService from './services/server-info/log/log_without_database.service';
 
 const app: express.Application = express();
-const port: string | number = process.env.PORT || 3000;
+const port: string | number = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../../clother-angular/dist/clother-angular')));
-
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', "true");
-
-    next();
-});
+app.use(cors());
 
 app.use('/api', api);
-
-app.get('**', (req: express.Request, res: express.Response) => {
-    res.status(200).sendFile(path.join(__dirname, '../../clother-angular/dist/clother-angular/index.html'));
-});
-
-
 
 const server: http.Server = http.createServer(app);
 server.listen(port, () => {
