7e1479bc23e7a26d6a47c6241668761cbcfb59d5
sergiy-ta <serhiy819@gmail.com>
2021-02-23 10:34:06 +0200

    created class photo of clothes database for save file

---
+++ clother-node/src/database/file/photo_of_clothes.database.ts
@@ -0,0 +1,80 @@
+import GridFsStorage from 'multer-gridfs-storage';
+import Grid from 'gridfs-stream';
+import crypto from 'crypto';
+import path from 'path';
+import Database from '../database';
+import { mongodbUrl } from '../database_info';
+import mongoose from 'mongoose';
+
+export default class PhotoOfClothesDatabase extends Database {
+    constructor() { 
+        super();
+    }
+
+    private async grid(): Promise<Grid.Grid> {
+        const conn = mongoose.createConnection(mongodbUrl);
+        const gfs: Grid.Grid = Grid((await conn).db, mongoose.mongo);
+        gfs.collection('uploads');
+        return gfs;
+    }
+
+    public save(): any {
+        this.grid();
+        return new GridFsStorage({
+            url: mongodbUrl,
+            file: (req, file) => {
+                return new Promise((resolve, reject) => {
+                    crypto.randomBytes(16, (err, buf) => {
+                        if (err) {
+                            return reject(err);
+                        }
+                        const filename = buf.toString('hex') + path.extname(file.originalname);
+                        const fileInfo = {
+                            filename: filename,
+                            bucketName: 'uploads'
+                        };
+                        resolve(fileInfo);
+                    });
+                });
+            }
+        });
+    }
+
+    public get(info: { filename: string }): Promise<any> {
+        return new Promise<any>(async (resolve,  reject) => {
+            (await this.grid()).files.findOne({ filename: info.filename }, async (err, file) => {
+                if (!file || file.length === 0) {
+                    return resolve(false);
+                }
+
+                if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+                    const readstream = (await this.grid()).createReadStream(file.filename);
+                    resolve(readstream);
+                } else {
+                    resolve(false);
+                }
+            });
+        });
+    }
+
+    public getList(): Promise<boolean | any[]> {
+        return new Promise<boolean | any[]>(async (resolve, reject) => {
+            (await this.grid()).files.find().toArray((err, files) => {
+                console.log(err, files);
+                
+                if (!files || files.length === 0) {
+                    resolve(false);
+                } else {
+                    files.map(file => {
+                        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+                            file.isImage = true;
+                        } else {
+                            file.isImage = false;
+                        }
+                    });
+                    resolve(files);
+                }
+            });
+        });
+    }
+}
