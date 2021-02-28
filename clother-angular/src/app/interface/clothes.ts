75412159bf3e51c99561ca9f41977ca002fa9d72
sergiy-ta <serhiy819@gmail.com>
2021-02-28 23:26:25 +0200

    added interface of clothes

---
+++ clother-angular/src/app/interface/clothes.ts
@@ -0,0 +1,14 @@
+import { User } from "./user";
+
+export interface Clothes {
+    readonly _id: string;
+    name: string;
+    photoName: string;
+    urlForBuy?: string;
+    infoOfClothes?: {
+        label: string;
+        probability: string;
+    }[];
+    readonly user: User | { _id: string; };
+    readonly dateOfCreation: Date;
+}
